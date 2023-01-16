2c154dfec8ba9ddbcc6a2e6769218093abd0a56e
vman241308 <vman241308@gmail.com>
2023-01-16 00:01:49 -0500

    first

---
+++ Neighbour/Neighbour/NeighbourApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeighbourApp.swift
+//  Neighbour
+//
+//  Created by fulldev on 1/14/23.
+//
+
+import SwiftUI
+
+@main
+struct NeighbourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
