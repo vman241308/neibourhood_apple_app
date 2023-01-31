0a918627740f416defc0763291da64f99e816689
vman241308 <vman241308@gmail.com>
2023-01-31 10:54:41 -0500

    focus

--- NeighborhoodTV/Description.swift
+++ NeighborhoodTV/Description.swift
@@ -15,7 +15,8 @@ struct Description: View {
     @Binding var isCornerScreenFocused:Bool
     @Binding var currentVideoTitle:String
     @Binding var currentVideoDescription:String
-    @State var isVideoSectionFocused = false
+    @Binding var isVideoSectionFocused:Bool
+    @Binding var isPreviewVideoStatus:Bool
     
     @FocusState private var nameInfocused: Bool
     
@@ -26,17 +27,13 @@ struct Description: View {
                     VStack {
                         HStack {
                             VStack(alignment: .leading) {
-                                VStack {
+                                VStack(alignment: .leading) {
                                     Text("Streaming Now - \(currentVideoTitle)")
                                         .font(.custom("Arial Round MT Bold", fixedSize: 35))
                                     
                                     Text("\(currentVideoDescription)")
                                         .font(.custom("Arial Round MT Bold", fixedSize: 25))
-                                }.focusable(true) {newState in isVideoSectionFocused = newState }
-                                    .onLongPressGesture(minimumDuration: 0.001, perform: {isFullScreenBtnClicked = true})
-                                    .onExitCommand(perform: {isFullScreenBtnClicked ? (isFullScreenBtnClicked = false ): (isCornerScreenFocused = true)})
-
-                                
+                                }.focusable(false)
                               
                                     Text("Watch in FullScreen").font(.custom("Arial Round MT Bold", fixedSize: 25))
                                     .padding(20)
@@ -48,23 +45,23 @@ struct Description: View {
                                     .focusable(true) {newState in isVideoSectionFocused = newState }
                                     .scaleEffect(isVideoSectionFocused ? 1.1 : 1)
                                     .onLongPressGesture(minimumDuration: 0.001, perform: {isFullScreenBtnClicked = true})
-                                    .onExitCommand(perform: {isFullScreenBtnClicked ? (isFullScreenBtnClicked = false ): (isCornerScreenFocused = true)})
                             }
                             Spacer()
                         }.padding(.top, 130)
                         Spacer()
                     }.frame(width: 950, height: 505)
+                        .onExitCommand(perform: {isFullScreenBtnClicked ? (isFullScreenBtnClicked = false ): (isCornerScreenFocused = true)})
                 }
                 
                 VStack {
                     PreviewVideo(currentVideoPlayURL: $currentVideoPlayURL)
                         .shadow(color: .black, radius: 10)
                         .frame(width: (isFullScreenBtnClicked ? 1920 : 900), height: (isFullScreenBtnClicked ? 1080 : 505))
-                        .focusable(true) {newState in isVideoSectionFocused = newState }
-                        .onLongPressGesture(minimumDuration: 0.001, perform: {isFullScreenBtnClicked = true})
-                        .onExitCommand(perform: {isFullScreenBtnClicked ? (isFullScreenBtnClicked = false ): (isCornerScreenFocused = true)})
+                        .focusable(false)
                     Spacer()
                 } .frame(width: 550, height: 505)
+                    .onExitCommand(perform: {isFullScreenBtnClicked ? (isFullScreenBtnClicked = false ): (isCornerScreenFocused = true)})
+                    
             }
             .frame(width: 1500, height: 505)
             .onExitCommand(perform: {isFullScreenBtnClicked ? (isFullScreenBtnClicked = false ): (isCornerScreenFocused = true)})
