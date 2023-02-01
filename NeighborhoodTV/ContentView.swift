136a2a533abcdec526596a81b2bd5978482744ca
vman241308 <vman241308@gmail.com>
2023-02-01 02:03:05 -0500

    when clicking up button, original video play URL

--- NeighborhoodTV/ContentView.swift
+++ NeighborhoodTV/ContentView.swift
@@ -19,8 +19,8 @@ struct ContentView: View {
     @State var isCornerScreenFocused = true
     @State var isTitleFocused = false
     @State var isVideoSectionFocused = false
-//    @State var descriptionPreviewVideo: PreviewVideo = PreviewVideo(currentVideoPlayURL: )
-
+    //    @State var descriptionPreviewVideo: PreviewVideo = PreviewVideo(currentVideoPlayURL: )
+    
     @State var isLocationItemFocused:Int = 0
     @State var currentVideoDescription:String =  (UserDefaults.standard.object(forKey: "currentVideoDescription") as? String ?? "")
     
@@ -51,50 +51,48 @@ struct ContentView: View {
                             
                             if isCornerScreenFocused {
                                 Divider()
-                                    .focusable(isPreviewVideoStatus ? isVideoSectionFocused ? false : true : false) {newState in isTitleFocused = newState; isPreviewVideoStatus =  false }
+                                    .focusable(isPreviewVideoStatus ? isVideoSectionFocused ? false : true : false) {newState in isTitleFocused = newState; onUpButtonToHome() }
                             }
                             else {
                                 Divider()
                                     .focusable( !isVideoSectionFocused ? true : false ) { newState in isTitleFocused = newState ; isVideoSectionFocused = true  }
                                     .onExitCommand(perform: {isFullScreenBtnClicked ? (isFullScreenBtnClicked = false ): (isCornerScreenFocused = true)})
                             }
-                           
-                            
                             MediaList(allMediaItems:$allMediaItems, isPreviewVideoStatus : $isPreviewVideoStatus, isCornerScreenFocused:$isCornerScreenFocused, currentVideoTitle:$currentVideoTitle, currentVideoDescription:$currentVideoDescription, currentVideoPlayURL:$currentVideoPlayURL)
                         }
                         .onExitCommand(perform: {
                             if !self.isCornerScreenFocused {
                                 isCornerScreenFocused = true
-                            } else {
-                                
-                                guard let _originalVideoPlayURL = UserDefaults.standard.object(forKey: "original_uri") as? String else {
-                                    print("Invalid URL")
-                                    return
-                                }
-                                
-                                guard let _currentVideoTitle = UserDefaults.standard.object(forKey: "original_title") as? String else {
-                                    print("Invalid Title")
-                                    return
-                                }
-                                
-                                DispatchQueue.main.async {
-                                    NotificationCenter.default.post(name: .dataDidFlow, object: _originalVideoPlayURL)
-                                }
-                                
-                                currentVideoPlayURL = _originalVideoPlayURL
-                                currentVideoTitle = _currentVideoTitle
-                                isPreviewVideoStatus = false
-                            }
+                            } else { onUpButtonToHome() }
                         })
                         .frame(width: 1500, height: (isPreviewVideoStatus ? isCornerScreenFocused ?  960 : 500 : 200))
                     }
                 }
-                
             }
             .background(Image("bg_full")
                 .resizable()
                 .frame(width: 1920, height: 1080, alignment: .center))
         }
+    }
+    
+    func onUpButtonToHome () {
+        guard let _originalVideoPlayURL = UserDefaults.standard.object(forKey: "original_uri") as? String else {
+            print("Invalid URL")
+            return
+        }
+        
+        guard let _currentVideoTitle = UserDefaults.standard.object(forKey: "original_title") as? String else {
+            print("Invalid Title")
+            return
+        }
+        
+        DispatchQueue.main.async {
+            NotificationCenter.default.post(name: .dataDidFlow, object: _originalVideoPlayURL)
+        }
+        
+        currentVideoPlayURL = _originalVideoPlayURL
+        currentVideoTitle = _currentVideoTitle
+        isPreviewVideoStatus = false
     }
 }
 
